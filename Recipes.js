8119fe6397d053cc5d4d16b4f52c41c4a64a9bbf
Superbomb <a13583321659@126.com>
2025-05-23 23:58:41 +0800

    Bug修复

--- Recipes.js
+++ Recipes.js
@@ -1,13 +1,11 @@
-ServerEvents.recipes(allthemods => {
-
-		allthemods.shapeless(Item.of('mtr:apg_door', 8),
-		  [
-		    'c:redstone_block',
-		    'minecraft:iron_door',
-		    'c:glowstone_dust',
+ServerEvents.recipes(mtr => {
+	mtr.shapeless(
+		Item.of('mtr:apg_door', 8),
+		[
+			'minecraft::redstone_block',
+			'minecraft:iron_door',
+			'minecraft:glowstone_dust',
 			'minecraft:glass_pane'
-		  ]
-		)
-
-	}
-)
+		]
+	)
+})

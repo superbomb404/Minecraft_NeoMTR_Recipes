c4669149c4cb535325a657596ea0a80cf8fbb5c8
Superbomb <a13583321659@126.com>
2025-05-23 19:27:36 +0800

    代码格式范例

---
+++ Recipes.js
@@ -0,0 +1,19 @@
+// This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10.
+// As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
+
+ServerEvents.recipes(allthemods => {
+		allthemods.recipes.kubejs.shaped('mtr:dashboard_2', 
+			[
+				"III",
+				"RGL",
+				"III"
+			],
+			{
+				I: 'minecraft:planks',
+				R: 'minecraft:redstone_dusts',
+				G: 'minecraft:glass_pane',
+				L: 'minecraft:glowstone_dusts'
+			}
+		)
+	}
+)

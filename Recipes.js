583adb8f1a68eec8fbfe27fb54f537a9238ab9e0
qiqizaoxiao <911761856@qq.com>
2025-05-23 23:06:35 +0800

    Update Recipes.js

--- Recipes.js
+++ Recipes.js
@@ -14,20 +14,3 @@ ServerEvents.recipes(allthemods => {
 		)
 	}
 )
-
-ServerEvents.recipes(allthemods => {
-		allthemods.recipes.kubejs.shaped('mtr:dashboard', 
-			[
-				"III",
-				"RGL",
-				"III"
-			],
-			{
-				I: 'minecraft:planks',
-				R: 'minecraft:redstone_dusts',
-				G: 'minecraft:glass_pane',
-				L: 'minecraft:glowstone_dusts'
-			}
-		)
-	}
-)

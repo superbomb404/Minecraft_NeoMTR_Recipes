52e7a263ba5739c097a34cc1f9c56a06ec590364
qiqizaoxiao <911761856@qq.com>
2025-05-24 00:45:23 +0800

    Update Recipes.js

--- Recipes.js
+++ Recipes.js
@@ -2,7 +2,7 @@ ServerEvents.recipes(mtr => {
 	mtr.shapeless(
 		Item.of('mtr:apg_door', 8),
 		[
-			'minecraft::redstone_block',
+			'minecraft:redstone_block',
 			'minecraft:iron_door',
 			'minecraft:glowstone_dust',
 			'minecraft:glass_pane'

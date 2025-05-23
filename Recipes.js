677fb6deb2096cabc72e3a218f97034d442ad852
qiqizaoxiao <911761856@qq.com>
2025-05-23 23:38:15 +0800

    Update Recipes.js

--- Recipes.js
+++ Recipes.js
@@ -2,14 +2,12 @@ ServerEvents.recipes(allthemods => {
 
 		allthemods.shapeless(Item.of('mtr:apg_door', 8),
 		  [
-		    'c:redstone_blocks',
+		    'c:redstone_block',
 		    'minecraft:iron_door',
-		    'c:glowstone_dusts',
+		    'c:glowstone_dust',
 			'minecraft:glass_pane'
 		  ]
 		)
-
-
 
 	}
 )
